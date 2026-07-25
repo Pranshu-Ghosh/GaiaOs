9c2a9db003f1d5afb12c9f356566b26867e0c595
Pranshu-Ghosh <pranshughosh881@gmail.com>
2026-07-25 12:55:48 +0530

    fixed gitignore

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, status
+from mockdata import users
 
 
 app= FastAPI()
@@ -18,6 +19,32 @@ def about():
     return {"name":"Gaia OS",
     "version":"0.1",
     "author":"Pranshu Ghosh"}
+
+
+@app.get("/users")
+def get_users():
+    return users 
+
+@app.get("/users/{id}")
+def get_user(id:int):
+    if id>len(users) or id<=0:
+        return {"message":"user not found"}
+    for user in users:
+        if user["id"]==id:
+            return user
+   
+
+@app.post("/create_user")
+def create_user(name:str,email:str):
+    users.append({"id":len(users)+1,"name":name,"email":email})
+    return users
+
+
+
+
+
+
+
 
 
 @app.get("/health",status_code=status.HTTP_200_OK)
