279367dc7f397bc2e31ecc6a929b29c1da69fb1f
Pranshu-Ghosh <pranshughosh881@gmail.com>
2026-07-25 12:54:13 +0530

    added post endpoint and a mockdatabase

---
+++ mockdata.py
@@ -0,0 +1,12 @@
+users=[
+    {
+        "id":1,
+        "name":"Pranshu Ghosh",
+        "email":"[EMAIL_ADDRESS1]"
+    },
+    {
+        "id":2,
+        "name":"Rishav Ghosh",
+        "email":"[EMAIL_ADDRESS2]"
+    }
+]
