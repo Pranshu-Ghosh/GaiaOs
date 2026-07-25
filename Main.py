a5b839eb924fd8624e11e8d4f1c1d5a047dbf649
Pranshu-Ghosh <pranshughosh881@gmail.com>
2026-07-25 11:19:21 +0530

    add more endpoints

--- Main.py
+++ Main.py
@@ -1,10 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+
 
 app= FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+
 @app.get("/home")    
 def home():
-    return "Welcome to Gaia OS " 
+    return {"Message":"Welcome to Gaia OS"} 
+
+@app.get("/about")    
+def about():
+    return {"name":"Gaia OS",
+    "version":"0.1",
+    "author":"Pranshu Ghosh"}
+
+
+@app.get("/health",status_code=status.HTTP_200_OK)
+def health_check():
+    return {"status":"ok"}
